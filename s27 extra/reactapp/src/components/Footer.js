f3a21599988adff316f2422cb866a3dff525906b
Dahl Tamares <dahltamares89@gmail.com>
2022-12-14 18:40:41 +0800

    react extra

---
+++ s27 extra/reactapp/src/components/Footer.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const Footer = () => {
+
+  return (
+
+    <footer>
+
+        <p>Copyright &copy; 2022</p>
+
+        <a href='/about'>About</a>
+
+    </footer>
+
+  )
+
+}
+
+export default Footer
